c523346f3ad28d293702d70141c31fa3bc7fe7a3
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-29 17:06:07 +0300

    changed width

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -3,7 +3,7 @@ import { bannerImg } from "../../assets/index";
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
+    <div className="w-500 lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[600px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
        src={bannerImg}
