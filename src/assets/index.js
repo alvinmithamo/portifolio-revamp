bd8023cbdee191fed59f6c8752d971d73d05f61e
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-04-29 17:05:18 +0300

    page routing

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,8 +3,13 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
+import projectFour from "./images/projects/projctFour.png"
+import projectFive from "./images/projects/projectFive.png"
+import projectSeven from "./images/projects/ProjectSeven.png"
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import testimonialThree from "./images/testmonial/testimonialThree.png";
+import testimonialFour from "./images/testmonial/testimonialFour.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 
@@ -14,8 +19,13 @@ export {
   projectOne,
   projectTwo,
   projectThree,
+  projectFour,
+  projectFive,
+  projectSeven,
   testimonialOne,
   testimonialTwo,
+  testimonialThree,
+  testimonialFour,
   quote,
   contactImg,
 };
