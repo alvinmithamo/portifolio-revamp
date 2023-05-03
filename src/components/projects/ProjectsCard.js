24cb434e1f9e261c06b5cea03f3cde77aec46aa7
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-05-04 02:55:38 +0300

    added links

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -19,12 +19,14 @@ const ProjectsCard = ({ title, des, src }) => {
               {title}
             </h3>
             <div className="flex gap-2">
+              <a href="https://github.com/alvinsleek/" target='blank'>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <BsGithub />
               </span>
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+              </a>
+              {/* <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <FaGlobe />
-              </span>
+              </span> */}
             </div>
           </div>
           <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
