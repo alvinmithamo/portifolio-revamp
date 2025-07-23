7c9b42f953b9cd5b34d514951630de1b57c56470
alvinmithamo <alvinmithamo1@gmail.com>
2025-07-23 14:00:44 +0300

    added ga4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import "slick-carousel/slick/slick.css"; 
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-S4D3KL3K4G'); // Replace with your actual Google Analytics ID
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +13,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
