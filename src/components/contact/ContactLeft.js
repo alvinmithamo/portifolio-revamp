7de37c4d0d75438f0e7f810b7ff1e41196cdc4f9
alvinmithamo <alvinmithamo1@gmail.com>
2025-07-28 02:07:30 +0300

    updated photo

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaWhatsapp, FaLocationArrow, FaSearchLocation, FaLinkedin, FaGithub } from "react-icons/fa";
-import { contactImg } from "../../assets/index";
+import { profile1 } from "../../assets/index";
 
 const ContactLeft = () => {
   return (
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
         className="w-full h-64 object-cover rounded-lg mb-2"
-        src={contactImg}
+        src={profile1}
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
