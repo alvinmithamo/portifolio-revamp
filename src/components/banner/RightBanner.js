8f610a7fd17f368ffcea26727940b4e7e80011c5
alvinmithamo <alvinmithamo1@gmail.com>
2025-07-28 02:07:16 +0300

    changed profile photo position

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,11 +5,12 @@ const RightBanner = () => {
   return (
     <div className="w-500 lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[50px] h-[50px] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[50px] h-[50px] sm:w-[300px] sm:h-[300px] lgl:w-[500px] lgl:h-[500px] z-10 rounded-full object-cover border-4 border-[#1e2024]"
        src={profile1}
         alt=""
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px]
+       bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center rounded-full"></div>
     </div>
   );
 }
