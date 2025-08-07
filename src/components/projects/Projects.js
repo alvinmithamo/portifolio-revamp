e143210c81af75bd256ceaa56d33de3d414e6e30
alvinmithamo <alvinmithamo1@gmail.com>
2025-08-07 11:52:41 +0300

    updated projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectFour, projectFive, projectSix, projectSeven, projectEight, projectNine, projectTen, projectEleven, projectTwelve, projectThirteen, projectFourteen } from "../../assets/index";
+import { projectOne, projectFour, projectFive, projectSix, projectSeven, projectEight, projectNine, projectTen, projectEleven, projectTwelve, projectThirteen, projectFifteen } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 import { Link } from 'react-dom';
 
@@ -20,20 +20,21 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="WOMAN HEALTH AFRICA"
-          des="This is a portfolio website for a non-governmental organisation which is committed to enhancing the health and well-being of African women during midlife and beyond. "
-          src={projectFourteen}
-          link="https://www.womanhealthafrica.org/"
+          title="KIBWEZI HONEY MARKETING SOCIETY"
+          des="This is a website for a women-led co-operative society which is committed to empowering bee-keeping farming in Makueni. "
+          src={projectFifteen}
+          link="https://kibwezihoneymarketing.com/"
         />
 
 
         <ProjectsCard
-              title="NJERAE"
-              des="This is a music artist portfolio website which portrays what she does in the music industry. "
-              src={projectTen}
-              link="https://njerae.vercel.app/"
-          />
-
+          title="VOI ADVENTURES"
+          des="This is a website for a company that offers tours and travel services to various destinations in Kenya and beyond. "
+          src={projectTwelve}
+          link="https://voiadventures.com/"
+        />
+        
+        
         <ProjectsCard
           title="DENIS MOROGA & ASSOCIATES ADVOCATES "
           des=" This is a law firm website which serves as their portfolio, showcasing the various legal services they offer."
@@ -41,6 +42,7 @@ const Projects = () => {
           link="https://morogalaw.co.ke"
 
           />
+
 
         <ProjectsCard
          
@@ -51,11 +53,20 @@ const Projects = () => {
         />
 
 
+
         <ProjectsCard
-          title="SPEEDEX INTERIORS"
-          des=" The website is an interior design website which specializes in advanced 3D Product Rendering Services."
-          src={projectFour}
-          link="https://speedexinteriors.co.ke/"
+          title="MOUNT KENYA WILDLIFE ESTATE"
+          des="This is a website for a luxurious hotel in the Mount Kenya region which offers accommodation for its clientele; both long-term and short term.  "
+          src={projectEleven}
+          link="https://mountkenyawe.co.ke/"
+        />    
+
+
+          <ProjectsCard
+              title="NJERAE"
+              des="This is a music artist portfolio website which portrays what she does in the music industry. "
+              src={projectTen}
+              link="https://njerae.vercel.app/"
           />
          
         
@@ -84,26 +95,20 @@ const Projects = () => {
           link="https://olkondosafaris.com"
         />
 
+
+
+        <ProjectsCard
+          title="SPEEDEX INTERIORS"
+          des=" The website is an interior design website which specializes in advanced 3D Product Rendering Services."
+          src={projectFour}
+          link="https://speedexinteriors.co.ke/"
+          />
+
         <ProjectsCard
           title="SHIRINI EXP"
           des="This is an agency website that mainly offers services in experiential marketing and communication"
           src={projectNine}
           link="https://shirini.co.ke/"
-        />
-
-        
-        <ProjectsCard
-          title="MOUNT KENYA WILDLIFE ESTATE"
-          des="This is a website for a luxurious hotel in the Mount Kenya region which offers accommodation for its clientele; both long-term and short term.  "
-          src={projectEleven}
-          link="https://mountkenyawe.co.ke/"
-        />
-
-        <ProjectsCard
-          title="VOI ADVENTURES"
-          des="This is a website for a company that offers tours and travel services to various destinations in Kenya and beyond. "
-          src={projectTwelve}
-          link="https://voiadventures.com/"
         />
 
         <ProjectsCard
